ec6a3e51af791168a0c7fcd9c95a5ddcf4edaa32
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-07 07:13:02 -0700

    task 3 and 4 passed

--- models/base_model.py
+++ models/base_model.py
@@ -3,8 +3,7 @@
 
 
 import uuid
-from datetime import datetime
-from datetime import datetime, timezone
+from datetime import datetime  # , timezone
 
 
 class BaseModel():
@@ -12,16 +11,28 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         """ instance intialization function """
-        # id: string - via uuid when an instance is created
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now(tz=timezone.utc)
-        self.updated_at = datetime.now(tz=timezone.utc)
+        if kwargs and len(kwargs) > 0:
+            for key, val in kwargs.items():
+                if key == 'created_at':
+                    dtime_obj_val = self.convert_to_datetime(val)
+                    setattr(self, key, dtime_obj_val)
+                elif key == 'updated_at':
+                    dtime_obj_val = self.convert_to_datetime(val)
+                    setattr(self, key, dtime_obj_val)
+                elif key != '__class__':
+                    setattr(self, key, val)
+
+        else:
+            # id: string - via uuid when an instance is created
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """ save function to set current time
         for instance attribute updated at
         """
-        self.updated_at = datetime.now(tz=timezone.utc)
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
@@ -39,3 +50,14 @@ class BaseModel():
         """ return : string as required by task """
         return ("[{}] ({}) {}".format(type(self).__name__,
                 self.id, self.__dict__))
+
+    @staticmethod
+    def convert_to_datetime(datetime_string):
+        """ Return: datetime object
+        convert string to datatime object
+        via datetime.strptime
+        """
+        dtime_string = str(datetime_string)
+        dtime_format = "%Y-%m-%dT%H:%M:%S.%f"
+        dtime_obj = datetime.strptime(dtime_string, dtime_format)
+        return (dtime_obj)
