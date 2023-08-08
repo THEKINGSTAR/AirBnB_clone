f9d05758c916518dc507e0db838fa0f61acece8a
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-08 01:36:27 -0700

    fix task 5

--- models/base_model.py
+++ models/base_model.py
@@ -28,6 +28,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def save(self):
         """ save function to set current time
