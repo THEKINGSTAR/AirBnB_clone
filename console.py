27221adfd1d6e0dfc141d0ecbf3540abc3bc22cb
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-09 07:16:42 -0700

    task 7 , handle create command for BaseModel

--- console.py
+++ console.py
@@ -19,6 +19,44 @@ class HBNBCommand(cmd.Cmd):
         """ exit on EOF (ctrl+D) """
         return True
 
+    def do_create(self, line):
+        """ creates instances(objs) of input class
+        also save it to json file (file.json)
+        Return: print the id of created instance 
+        example : create BaseModel
+        this will create instance of BaseModel class
+        """
+        # line is arguments or (just all text after main content)
+        # print(f"line is: {line}")
+        import importlib
+
+        if len(line) < 1:
+            print("** class name missing **")
+            return
+        # line is classname
+        snake_class_name = self.pascal_to_snake(line)
+        try:
+            module = importlib.import_module('models.' + snake_class_name)
+        except Exception:
+            print("** class doesn't exist **")
+            return
+        print("class does exist")
+
+
+    @staticmethod
+    def pascal_to_snake(name):
+        """ conver class name (pascal case)
+        to file name(snake case)
+        """
+        snake_name = name[0].lower()  # first cap letter
+        for char in name[1:]:
+            if char.isupper():
+                snake_name += '_' + char.lower()
+            else:
+                snake_name += char
+        return snake_name        
+        
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
