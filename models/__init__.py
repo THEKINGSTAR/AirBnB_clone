0d82b9b06bf40a812c4a39d675553316ecdcd6b5
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-08 17:47:43 -0700

    fix reload for task 5

--- models/__init__.py
+++ models/__init__.py
@@ -4,5 +4,4 @@
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
-if __name__ == "__main__":
-    storage.reload()
+storage.reload()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,7 +9,7 @@ deserializes JSON file to instances:
 
 import json
 import os
-
+from models import *  # Import all classes
 
 
 class FileStorage:
@@ -98,14 +98,27 @@ class FileStorage:
         """
         if not os.path.isfile(FileStorage.__file_path):
             return
-        
+        import importlib
         with open(FileStorage.__file_path, 'r', encoding="utf-8") as file:
             data = file.read()
             if data:
                 python_obj = json.loads(data)
                 for key, value in python_obj.items():
                     class_name, obj_id = key.split('.')
-                    module = __import__('models.' + class_name, fromlist=[class_name])
+                    snake_class_name = self.pascal_to_snake(class_name)
+                    # print(f"snake_class_name: {snake_class_name}")
+                    module = importlib.import_module('models.' + snake_class_name)
+                    # module = __import__('models.' + class_name, fromlist=[class_name])
                     cls = getattr(module, class_name)
                     obj = cls(**value)
                     FileStorage.__objects[key] = obj
+
+    @staticmethod
+    def pascal_to_snake(name):
+        snake_name = name[0].lower()  # first cap letter
+        for char in name[1:]:
+            if char.isupper():
+                snake_name += '_' + char.lower()
+            else:
+                snake_name += char
+        return snake_name
