de01f5c954d7248aa5e421380225457ef17e5863
THEKINGSTAR <thekingstar5002@gmail.com>
2023-08-12 04:58:29 +0300

    make all test for BASEMODEL1

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-THIS MODULE CONTAIN ONLY TOW FUNDCITONS TO WRITE OBJECT INTO JSON FILE
+THIS MODULE CONTAIN THE ENGINE FUNDCITONS TO WRITE OBJECT INTO JSON FILE
 Class FileStorage that serializes instances to a JSON file
 and
 deserializes JSON file to instances:
@@ -15,25 +15,7 @@ import importlib
 
 class FileStorage:
     """
-    Private class attributes:
-    __file_path: string - path to the JSON file (ex: file.json)
-
-    __objects: dictionary - empty but will store all objects by <class name>.id
-    (ex: to store a BaseModel object with id=12121212, the key will
-    be BaseModel.12121212)
-
-    Public instance methods:
-
-    all(self): returns the dictionary __objects
-
-    new(self, obj): sets in __objects the obj with key <obj class name>.id
-
-    save(self): serializes __objects to the JSON file (path: __file_path)
-
-    reload(self): deserializes the JSON file to __objects
-
-    (only if the JSON file (__file_path) exists ; otherwise, do nothing.
-    If the file doesn’t exist, no exception should be raised)
+    Class FileStorage that serializes instances to a JSON file
     """
     __file_path = "file.json"
     __objects = {}
@@ -91,7 +73,8 @@ class FileStorage:
 
     @staticmethod
     def pascal_to_snake(name):
-        """ conver class name (pascal case)
+        """
+        conver class name (pascal case)
         to file name(snake case)
         """
         snake_name = name[0].lower()  # first cap letter
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,20 +1,44 @@
 #!/usr/bin/python3
-""" module description : testunit for *.py in python
+"""
+MODEL BALSE TEST CASES 
 """
 
 
 import unittest
+from unittest import TestCase
+from models.base_model import BaseModel
+from datetime import datetime
 
 
 class Test(unittest.TestCase):
-    """ desc---ription """
-
-    def testc1(self):
-        """ desc.....ription """
+    """
+    TEST FOR THE CLASS INHERITS AND ITS ATRRIUTES
+    """
+    
+    def test_base_model(self):
+        """
+        CHECK THE INHERETANCE AND THE ATRIBUTES
+        WHEN CREATING SUB CLASES
+        """
         # result = function_to_test(input_data)
         # self.assertEqual(result, expected_output)
-        pass
-
+        
+        base1 = BaseModel()
+        base2 = BaseModel()
+        self.assertNotEqual(base1.id, base2.id)
+        
+        base1.name = "ABDEL-MOHSEN"
+        base2.name = "KHALED"
+        self.assertNotEqual(base1.name, "KHALED")
+        self.assertEqual(base1.name, "ABDEL-MOHSEN")
+        self.assertEqual(base2.name, "KHALED")
+        self.assertTrue(isinstance(base1.id, str))
+        self.assertTrue(isinstance(base2.name, str))
+        self.assertTrue(isinstance(base2.to_dict(), dict))
+        self.assertTrue(isinstance(base1.created_at, datetime))
+        self.assertTrue(isinstance(base1.updated_at, datetime))
+        self.assertTrue(isinstance(base2.created_at, datetime))
+        self.assertTrue(isinstance(base2.updated_at, datetime))
 
 if __name__ == "__main__":
     unittest.main()
